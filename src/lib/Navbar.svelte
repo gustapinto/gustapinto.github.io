da6ad9abc301d9036b8da60d8d8b4657891dc34a
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-06-24 21:35:47 -0300

    Fix navbar on mobile

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,5 @@
-<nav class="w-100 -skew-y-2 origin-left px-[2em] pt-[2.5em] pb-[.25em] bg-primary">
-  <span class="text-6xl font-bold">
+<nav class="w-100 -skew-y-2 origin-left px-[1em] sm:px-[2em] pt-[1.5em] sm:pt-[2.5em] pb-[.25em] bg-primary">
+  <span class="text-4xl sm:text-6xl font-bold">
     Gustavo Henrique Pinto
   </span>
 </nav>
