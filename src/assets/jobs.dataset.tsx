e5ced3c143b655b9449505ee6f5146a64a719bb8
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-02-26 19:03:39 -0300

    improv: improve datasets

--- src/assets/jobs.dataset.tsx
+++ src/assets/jobs.dataset.tsx
@@ -1,4 +1,4 @@
-import {SiDocker, SiGo, SiJava, SiLaravel, SiPython, SiReact, SiVuedotjs} from 'react-icons/si';
+import {SiApacheairflow, SiDocker, SiGo, SiJava, SiLaravel, SiPython, SiReact, SiVuedotjs} from 'react-icons/si';
 import Highlight from '../components/text/Highlight';
 import { JobSchema } from '../schemas/jobs';
 
@@ -9,17 +9,17 @@ export const jobsDataset: Array<JobSchema> = [
     link: 'https://www.nappsolutions.com.br/',
     history: [
       {
-        role: 'Desenvolvedor fullstack sênior',
+        role: 'Desenvolvedor Fullstack Sênior',
         date: 'Novembro/2022 - Atualmente',
         description: <>
-          Atuo como desenvolvedor fullstack de <Highlight>microsserviços</Highlight> e plataformas web orientadas à eventos com as linguagens de programação <Highlight>Python</Highlight>, <Highlight>Java</Highlight> e <Highlight>Go</Highlight>, apoiadas por ferramentas como <Highlight>Camunda BPMN</Highlight> e serviços Cloud com GCP.
+          Atuo desenvolvendo microsserviços e plataformas web utilizando <Highlight>Java</Highlight>, <Highlight>Go</Highlight>, <Highlight>Laravel</Highlight> e <Highlight>React</Highlight>, apoiadas por ferramentas como <Highlight>Camunda BPMN</Highlight> e serviços da <Highlight>Google Cloud Platform</Highlight>.
         </>,
       },
       {
-        role: 'Analista de integração ETL pleno',
+        role: 'Analista de Integração Pleno',
         date: 'Dezembro/2021 - Novembro/2022',
         description: <>
-          Atuei como analista de integração <Highlight>ETL</Highlight>, desenvolvendo rotinas de ingestão de dados em <Highlight>Python</Highlight> e produzindo serviços em <Highlight>Go</Highlight>, promovendo a migração de rotinas legado para arquiteturas de ETL distribuidas e <Highlight>altamente concorrentes</Highlight>.
+          Atuei Desenvolvendo rotinas de ingestão de dados em <Highlight>Python</Highlight> e promovendo a migração de rotinas legado para arquiteturas distribuidas e altamente concorrentes usando serviços de ETL "Ad hoc" escritos em <Highlight>Go</Highlight> e apoiados pela plataforma de dados <Highlight>Hazelcast</Highlight>.
         </>,
       },
     ],
@@ -37,10 +37,10 @@ export const jobsDataset: Array<JobSchema> = [
     link: 'https://www.cassum.dev/',
     history: [
       {
-        role: 'Desenvolvedor fullstack jûnior',
+        role: 'Desenvolvedor Fullstack Junior',
         date: 'Dezembro/2019 - Novembro/2021',
         description: <>
-          Trabalhei como desenvolvedor web full stack na construção de <Highlight>aplicações web</Highlight> e SPAs de apoio a produção construídas em <Highlight>Laravel</Highlight> e <Highlight>Vue.js</Highlight>, tendo também a oportunidade de atuar com <Highlight>Python</Highlight> no desenvolvimento de rotinas ETL estruturadas com Flask.
+          Trabalhei contruindo e mantendo <Highlight>aplicações web</Highlight> e SPAs de apoio a produção com <Highlight>Laravel</Highlight> e <Highlight>Vue.js</Highlight>, tendo trabalhado também com <Highlight>Python</Highlight> no desenvolvimento de rotinas <Highlight>ETL</Highlight>.
         </>
       }
     ],
