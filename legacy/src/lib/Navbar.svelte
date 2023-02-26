167ff26ba55457cbdb178dfc266f1d4d6ecae1fb
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-02-26 19:04:05 -0300

    chore: remove legacy folder

--- legacy/src/lib/Navbar.svelte
+++
@@ -1,7 +0,0 @@
-<nav class="w-100 -skew-y-2 origin-left px-[1em] sm:px-[2em] pt-[1.5em] sm:pt-[2.5em] pb-[1em] bg-primary">
-  <a href="/">
-    <span class="text-4xl sm:text-6xl font-bold">
-      Gustavo Henrique Pinto
-    </span>
-  </a>
-</nav>
--- legacy/src/lib/templates/Card.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let _class: string
-
-  let cardClass: string = 'bg-white skew-y-2 p-6 ' + _class
-</script>
-
-<div class={cardClass}>
-  <slot />
-</div>
