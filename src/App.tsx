a5e3bb7cbbd417982ef7c7f6ec66fe672ef18fee
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-03-02 19:36:47 -0300

    fix: fix title and favicon.ico

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 export default function App() {
+  useEffect(() => {
+    document.title = 'Gustavo Pinto';
+  }, []);
+
   return (
     <>
       <Navbar />
